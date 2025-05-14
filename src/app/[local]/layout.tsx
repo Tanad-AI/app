a477f61716c3a8992f9a8fc5f4d7ad95fc8b2eec
LOLKTKAT <lolktkat@gmail.com>
2025-05-14 08:19:03 +0300

    fix: autoscaling safari issue

--- src/app/[local]/layout.tsx
+++ src/app/[local]/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
           <Head>
             <meta
               name="viewport"
-              content="width=device-width, initial-scale=1, maximum-scale=1"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
             />
           </Head>
           <body className={font.className}>
